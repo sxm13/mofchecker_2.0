f0ab1d7ecbe5bced341b8815cc09b85a665fa74f
Xin Jin <xin.jin@epfl.ch>
2025-02-10 17:13:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Au-4/mofchecker_2.0",
     license = "MIT",
-    license_files=["LICENSE"]，
+    license_files=["LICENSE"],
     packages=find_packages(),
     install_requires=[
         "click",
