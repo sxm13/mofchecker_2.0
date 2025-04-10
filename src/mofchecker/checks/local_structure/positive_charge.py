5db35071a0a395864553f248e40eb873ac9986a9
Xin Jin <xin.jin@epfl.ch>
2025-04-10 16:12:00 +0200

    Update positive_charge.py

--- src/mofchecker/checks/local_structure/positive_charge.py
+++ src/mofchecker/checks/local_structure/positive_charge.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """Check the positive charge from linkers like N with CN4 and O with CN3."""
 from pymatgen.analysis.graphs import StructureGraph
-
-from mofchecker.checker_types import StructureIStructureType
-
+from structuregraph_helpers.create import construct_clean_graph
+from mofchecker.types import StructureIStructureType
+import networkx as nx
 from .base_coordination_check import BaseCoordinationCheck
-from ..utils.get_indices import _is_any_neighbor_metal, get_n_indices, get_o_indices, get_ge_indices, get_sb_indices
+from ..utils.get_indices import num_neighbor_metal, get_n_indices, get_o_indices, get_ge_indices, get_sb_indices, is_metal
 
 
 class Positive_charge_Check(BaseCoordinationCheck):
@@ -41,14 +41,52 @@ class Positive_charge_Check(BaseCoordinationCheck):
     def _get_overcoordinated_nitrogen(self):
         """Check for all N if CN=4 and all O if CN = 3, ignore metal bonds."""
         overcoordinated_nitrogen_oxygen = []
-
+        N_sum = []
+        N_jump = []
+        cycles = []
+        nx_graph = construct_clean_graph(self.structure_graph)
+        simple_cycles = nx.simple_cycles(nx_graph, length_bound=16)
+        for cycle in simple_cycles:
+            cycles.append(cycle)
         for site_index in self.n_indices:
-            cn = self.get_cn(site_index)  # pylint:disable=invalid-name
-            if cn == 4 and not _is_any_neighbor_metal(self.get_connected_sites(site_index)):
-                overcoordinated_nitrogen_oxygen.append(site_index)
+            if site_index not in N_jump:
+                cn = self.get_cn(site_index)  # pylint:disable=invalid-name
+                neighbors = self.get_connected_sites(site_index)             
+                cm = num_neighbor_metal(neighbors)
+                if cn-cm == 4:
+                    overcoordinated_nitrogen_oxygen.append(site_index)
+                else:
+                    for cycle in cycles:
+                        N_possible_jump = []
+                        if len(cycle) == 16 and site_index in cycle and all( not is_metal(self.structure[ring]) for ring in cycle):
+                            num_n = 0
+                            num_c = 0
+                            num_n3 = 0
+                            for ring in cycle:
+                                if str(self.structure[ring].specie) == 'N':
+                                    N_possible_jump.append(ring)
+                                    num_n += 1
+                                    n_cn = self.get_cn(ring)
+                                    n_cm = num_neighbor_metal(self.get_connected_sites(ring))
+                                    if n_cn-n_cm == 3:
+                                        num_n3 += 1
+                                if str(self.structure[ring].specie) == 'C':
+                                    num_c += 1
+                            if (num_n == 4) and (num_n3 == 4) and (num_c == 12):
+                                overcoordinated_nitrogen_oxygen.append(site_index)
+                                overcoordinated_nitrogen_oxygen.append(site_index)
+                            if (num_n == 4) and (num_n3 == 3) and (num_c == 12):
+                                overcoordinated_nitrogen_oxygen.append(site_index)
+                            if (num_n == 4) and (num_c == 12):
+                                for n in N_possible_jump:
+                                    N_jump.append(n)
+                                break
+        
         for site_index in self.o_indices:
-            cn = self.get_cn(site_index)  # pylint:disable=invalid-name
-            if cn == 3 and not _is_any_neighbor_metal(self.get_connected_sites(site_index)):
+            cn = self.get_cn(site_index)
+            neighbors = self.get_connected_sites(site_index)             
+            cm = num_neighbor_metal(neighbors)  # pylint:disable=invalid-name
+            if cn-cm == 3:
                 overcoordinated_nitrogen_oxygen.append(site_index)
         for site_index in self.sb_indices:
             overcoordinated_nitrogen_oxygen.append(site_index)
