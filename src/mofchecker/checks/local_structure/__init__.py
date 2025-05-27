e9adfee5f7d3bda4d0d86c7e7a62701b0e5134a6
sxm13 <342247760@qq.com>
2025-05-27 21:42:18 +0900

    Update __init__.py

--- src/mofchecker/checks/local_structure/__init__.py
+++ src/mofchecker/checks/local_structure/__init__.py
@@ -11,5 +11,3 @@ from .undercoordinated_rare_earth import UnderCoordinatedRareEarthCheck  # noqa:
 from .positive_charge import Positive_charge_Check # noqa: F401
 from .negative_charge import Negative_charge_Check # noqa: F401
 from .fused_ring import Fusedring_Check # noqa: F401
-from .add_hydrogen import O_site_adding_hydrogen # noqa: F401
-from .add_linker import adding_linker # noqa: F401
